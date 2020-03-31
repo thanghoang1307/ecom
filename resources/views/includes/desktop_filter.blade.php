94ae483cf347d80de9206f79ce23a225c649134b
heromc <doanminhchien@gmail.com>
2020-03-31 12:18:21 +0700

    update id checkbox , radio

--- resources/views/includes/desktop_filter.blade.php
+++ resources/views/includes/desktop_filter.blade.php
@@ -12,8 +12,8 @@ $price_request = explode('_',app('request')->input('price'));
             <div class="category-option">
               @foreach ($brands as $brand)
               <div class="checkbox">
-                <input id="os-1" name="brand_checkbox" type="checkbox" value="{{$brand->id}}" {{in_array($brand->id,$brand_request) ? 'checked' : ''}}>
-                <label for="os-1" class="checkbox-label">
+                <input id="brand-desktop-{{$brand->id}}" name="brand_checkbox" type="checkbox" value="{{$brand->id}}" {{in_array($brand->id,$brand_request) ? 'checked' : ''}}>
+                <label for="brand-desktop-{{$brand->id}}" class="checkbox-label">
                   <img src="{{$brand->logo}}" height="20px">
                 </label>
               </div>
@@ -24,20 +24,20 @@ $price_request = explode('_',app('request')->input('price'));
             <h4 class="category-label">Giá bán</h4>
             <div class="category-option">
               <div class="checkbox">
-                <input id="price-1" name="price_checkbox" type="checkbox" value="0-5000000" {{in_array("0-5000000",$price_request) ? 'checked' : ''}}>
-                <label for="price-1" class="checkbox-label">Dưới 5 triệu</label>
+                <input id="price-desktop-1" name="price_checkbox" type="checkbox" value="0-5000000" {{in_array("0-5000000",$price_request) ? 'checked' : ''}}>
+                <label for="price-desktop-1" class="checkbox-label">Dưới 5 triệu</label>
               </div>
               <div class="checkbox">
-                <input id="price-2" name="price_checkbox" type="checkbox" value="5000000-7000000" {{in_array("5000000-7000000",$price_request) ? 'checked' : ''}}>
-                <label for="price-2" class="checkbox-label">Từ 5 - 7 triệu</label>
+                <input id="price-desktop-2" name="price_checkbox" type="checkbox" value="5000000-7000000" {{in_array("5000000-7000000",$price_request) ? 'checked' : ''}}>
+                <label for="price-desktop-2" class="checkbox-label">Từ 5 - 7 triệu</label>
               </div>
               <div class="checkbox">
-                <input id="price-3" name="price_checkbox" type="checkbox" value="7000000-10000000" {{in_array("7000000-10000000",$price_request) ? 'checked' : ''}}>
-                <label for="price-3" class="checkbox-label">Từ 7 - 10 triệu</label>
+                <input id="price-desktop-3" name="price_checkbox" type="checkbox" value="7000000-10000000" {{in_array("7000000-10000000",$price_request) ? 'checked' : ''}}>
+                <label for="price-desktop-3" class="checkbox-label">Từ 7 - 10 triệu</label>
               </div>
               <div class="checkbox">
-                <input id="price-4" name="price_checkbox" type="checkbox" value="10000000-1000000000000" {{in_array("10000000-1000000000000",$price_request) ? 'checked' : ''}}>
-                <label for="price-4" class="checkbox-label">Trên 10 triệu</label>
+                <input id="price-desktop-4" name="price_checkbox" type="checkbox" value="10000000-1000000000000" {{in_array("10000000-1000000000000",$price_request) ? 'checked' : ''}}>
+                <label for="price-desktop-4" class="checkbox-label">Trên 10 triệu</label>
               </div>
             </div>
           </div>
