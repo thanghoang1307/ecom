2f9a1bcdcddf06fd96321051801e544a4dea0115
heromc <doanminhchien@gmail.com>
2020-04-01 13:35:06 +0700

    updae báo giá html

--- resources/views/pdf/bao_gia.blade.php
+++ resources/views/pdf/bao_gia.blade.php
@@ -9,13 +9,13 @@
   <meta name="author" content="">
   <link rel="icon" href="assets/img/favicon.ico">
 
-  <title>Báo giá - One Stop Shop</title>
+  <title>Báo giá - OneStopShop.vn - Ngày: {{\Carbon\Carbon::now()->format('d/m/Y')}}</title>
 </head>
-<body style="line-height: 22px; color: #333; font-size: 14px; padding:40px;">
+<body style="line-height: 17px; color: #333; font-size: 14px; padding:10px;">
 	<table cellpadding="0" cellspacing="0" border="0" style="width: 100%;">
 		<tbody>
 			<tr>
-				<td width="50%"><p style="text-align: left;"><img style="height: 100px;" src="https://onestopshop.vn/assets/img/logo-color.png" alt="" /></p>
+				<td width="50%"><p style="text-align: left;"><a style="text-decoration: none;" href="https://onestopshop.vn/" target="_blank"><img style="height: 100px;" src="https://onestopshop.vn/assets/img/logo-color.png" alt="" /></a></p>
 				</td>
 				<td width="50%">
 					<p style="font-size: 20px; color: #3e9ef3; text-align: right; font-weight: bold;">CÔNG TY TNHH DỊCH VỤ UM</p>
@@ -30,13 +30,13 @@
 				</tr>
 				<tr>
 					<td colspan="2">
-						<p style="font-weight: bold;">Sub : Bảng chào giá các thiết bị và dịch vụ Công Nghệ Thông Tin</p>
-						<p>Kính gửi: Quý khách hàng,<br />
-						One-Stop-Shop xin gửi đến Quý khách hàng bảng chào giá các thiết bị và dịch vụ Công Nghệ Thông Tin như sau:</p>
+						<p style="font-weight: bold;">Bảng chào giá các thiết bị và dịch vụ Công Nghệ Thông Tin</p>
+						<p style="padding: 0; margin: 3px 0;">Kính gửi: Quý khách hàng,<br />
+						<a style="color: #962d91; text-decoration: none;" href="https://onestopshop.vn/" target="_blank">One-Stop-Shop</a> xin gửi đến Quý khách hàng bảng chào giá các thiết bị và dịch vụ Công Nghệ Thông Tin như sau:</p>
 					</td>
 				</tr>
 				<tr>
-					<td colspan="2"><p><strong>a).</strong> <strong style="text-decoration: underline;">Sản phẩm và dịch vụ:</strong></p>
+					<td colspan="2"><p style="padding: 0; margin: 3px 0;"><strong>a).</strong> <strong style="text-decoration: underline;">Sản phẩm và dịch vụ:</strong></p>
 					</td>
 				</tr>
 				<tr>
@@ -45,27 +45,29 @@
 						<table cellpadding="0" cellspacing="" border="1" style="width: 100%; -webkit-border-horizontal-spacing: 0; -webkit-border-vertical-spacing: 0;">
 							<tbody>
 								<tr style="background-color: #dedede;">
-									<td style="width: 2%"><p style="text-align: center; padding: 10px; margin: 0; font-weight: bold;">STT</p></td>
-									<td style="width: 48%"><p style="text-align: center; padding: 10px; margin: 0; font-weight: bold;">Mô tả thiết bị &amp; dịch vụ</p></td>
-									<td style="width: 10%"><p style="text-align: center; padding: 10px; margin: 0; font-weight: bold;">Bảo hành</p></td>
-									<td style="width: 10%"><p style="text-align: center; padding: 10px; margin: 0; font-weight: bold;">SL</p></td>
-									<td style="width: 15%"><p style="text-align: right; padding: 10px; margin: 0; font-weight: bold;">Đơn giá</p></td>
-									<td style="width: 15%"><p style="text-align: right; padding: 10px; margin: 0; font-weight: bold;">Thành tiền</p></td>
+									<td style="width: 2%"><p style="text-align: center; padding: 5px; margin: 3px 0; font-weight: bold;">STT</p></td>
+									<td style="width: 12%"><p style="text-align: center; padding: 5px; margin: 3px 0; font-weight: bold;">Mã thiết bị &amp; dịch vụ</p></td>
+									<td style="width: 40%"><p style="text-align: center; padding: 5px; margin: 3px 0; font-weight: bold;">Mô tả thiết bị &amp; dịch vụ</p></td>
+									<td style="width: 8%"><p style="text-align: center; padding: 5px; margin: 3px 0; font-weight: bold;">Bảo hành</p></td>
+									<td style="width: 7%"><p style="text-align: center; padding: 5px; margin: 3px 0; font-weight: bold;">SL</p></td>
+									<td style="width: 12%"><p style="text-align: right; padding: 5px; margin: 3px 0; font-weight: bold;">Đơn giá</p></td>
+									<td style="width: 20%"><p style="text-align: right; padding: 5px; margin: 3px 0; font-weight: bold;">Thành tiền</p></td>
 								</tr>
 								@foreach($prds as $prd)
-<tr>
-	<td style="width: 2%"><p style="text-align: center; padding: 10px; margin: 0;">{{$loop->iteration}}</p></td>
-									<td style="width: 48%"><p style="text-align: center; padding: 10px; margin: 0;">{{$prd->name}}</p></td>
-<td style="width: 10%"><p style="text-align: center; padding: 10px; margin: 0;">{{$prd->attrs()->where('code','bao-hanh')->first()->pivot->text_val ?? ''}}</p></td>
-									<td style="width: 10%"><p style="text-align: center; padding: 10px; margin: 0;">{{$carts[$prd->id]}}</p></td>
-									<td style="width: 15%"><p style="text-align: right; padding: 10px; margin: 0;">{{str_replace(".00", "",number_format($prd->current_price))}}<sup>đ</sup></p></td>
-									<td style="width: 15%"><p style="text-align: right; padding: 10px; margin: 0; font-weight: bold;">{{str_replace(".00", "",number_format($carts[$prd->id]*$prd->current_price))}}<sup>đ</sup></p></td>
-</tr>
-@endforeach
+								<tr>
+									<td style="width: 2%"><p style="text-align: center; padding: 5px; margin: 3px 0;">{{$loop->iteration}}</p></td>
+									<td style="width: 12%"><p style="text-align: center; padding: 5px; margin: 3px 0;"><a style="color: #333; text-decoration: none;" href="{{route('front.product-detail',$prd->slug)}}" target="_blank">{{$prd->sku}}</a></p></td>
+									<td style="width: 40%"><p style="text-align: center; padding: 5px; margin: 3px 0;"><a style="color: #333; text-decoration: none;" href="{{route('front.product-detail',$prd->slug)}}" target="_blank">{{$prd->name}}</a></p></td>
+									<td style="width: 8%"><p style="text-align: center; padding: 5px; margin: 3px 0;">{{$prd->attrs()->where('code','bao-hanh')->first()->pivot->text_val ?? ''}}</p></td>
+									<td style="width: 7%"><p style="text-align: center; padding: 5px; margin: 3px 0;">{{$carts[$prd->id]}}</p></td>
+									<td style="width: 12%"><p style="text-align: right; padding: 5px; margin: 3px 0;">{{str_replace(".00", "",number_format($prd->current_price))}}<sup>đ</sup></p></td>
+									<td style="width: 20%"><p style="text-align: right; padding: 5px; margin: 3px 0; font-weight: bold;">{{str_replace(".00", "",number_format($carts[$prd->id]*$prd->current_price))}}<sup>đ</sup></p></td>
+								</tr>
+								@endforeach
 								
 								<tr style="background-color: #dedede;">
-									<td colspan="5"><p style="text-align: right; padding: 10px; margin: 0; font-weight: bold;">Tổng</p></td>
-									<td><p style="text-align: right; padding: 10px; margin: 0; font-weight: bold;">{{str_replace(".00", "",number_format($total))}}<sup>đ</sup></p></td>
+									<td colspan="6"><p style="text-align: right; padding: 5px; margin: 3px 0; font-weight: bold;">Tổng</p></td>
+									<td><p style="text-align: right; padding: 5px; margin: 3px 0; font-weight: bold;">{{str_replace(".00", "",number_format($total))}}<sup>đ</sup></p></td>
 								</tr>
 							</tbody>
 						</table>
@@ -74,23 +76,23 @@
 				
 				
 				<tr>
-					<td colspan="2"><p><strong>b).</strong> <strong style="text-decoration: underline;"> Thời hạn hiệu lực của báo giá:</strong> Báo giá trên có hiệu lực trong vòng 07 ngày</p>
+					<td colspan="2"><p style="padding: 0; margin: 3px 0;"><strong>b).</strong> <strong style="text-decoration: underline;"> Thời hạn hiệu lực của báo giá:</strong> Báo giá trên có hiệu lực trong vòng 07 ngày</p>
 					</td>
 				</tr>
 				<tr>
-					<td colspan="2"><p><strong>c).</strong> <strong style="text-decoration: underline;">Hình thức thanh toán:</strong>  Bằng tiền mặt hoặc chuyển khoản</p>
+					<td colspan="2"><p style="padding: 0; margin: 3px 0;"><strong>c).</strong> <strong style="text-decoration: underline;">Hình thức thanh toán:</strong>  Bằng tiền mặt hoặc chuyển khoản</p>
 					</td>
 				</tr>
 				<tr>
-					<td colspan="2"><p><strong>d).</strong> <strong style="text-decoration: underline;">Thời gian giao hàng:</strong>  Từ 01 đến 03 ngày khi nhận được thông báo chuyển tiền</p>
+					<td colspan="2"><p style="padding: 0; margin: 3px 0;"><strong>d).</strong> <strong style="text-decoration: underline;">Thời gian giao hàng:</strong>  Từ 01 đến 03 ngày khi nhận được thông báo chuyển tiền</p>
 					</td>
 				</tr>
 				<tr>
-					<td colspan="2"><p><strong>e).</strong> <strong style="text-decoration: underline;">Điều kiện chung:</strong> Áp dụng theo chính sách bán hàng và quy định chung của One-Stop-Shop</p>
+					<td colspan="2"><p style="padding: 0; margin: 3px 0;"><strong>e).</strong> <strong style="text-decoration: underline;">Điều kiện chung:</strong> Áp dụng theo chính sách bán hàng và quy định chung của One-Stop-Shop</p>
 					</td>
 				</tr>
 				<tr>
-					<td colspan="2"><p>Chân thành cảm ơn và trân trọng kính chào!</p>
+					<td colspan="2"><p style="padding: 0; margin: 3px 0;">Chân thành cảm ơn và trân trọng kính chào!</p>
 					</td>
 				</tr>
 			</tr>
