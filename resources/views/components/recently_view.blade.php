40bb786c38769d0db1cea05592a4810c9b577ea1
heromc <doanminhchien@gmail.com>
2020-04-02 10:14:27 +0700

    fix lỗi giao diện

--- resources/views/components/recently_view.blade.php
+++ resources/views/components/recently_view.blade.php
@@ -6,37 +6,39 @@
           <h2 class="page-title">Bạn đã xem</h2>
           <div id="recently-viewed-slider" class="product-list owl-carousel owl-theme">
           	@foreach ($prds as $prd)
-            @if($prd->is_show)
-            <div class="item">
-              <a href="{{route('front.product-detail',$prd->slug)}}">
-                <div class="product-item">
-                  <div class="product-image">
-                    <figure>
-                      <img src="{{$prd->thumb}}">
-                    </figure>
-                  </div>
-                  <div class="product-text">
-                  <h3 class="product-name">{{$prd->name}}</h3>
-                  <h4 class="product-price price">@if($prd->regular_price || $prd->current_price)
-                    {{$prd->sale_price ? $prd->sale_price : $prd->regular_price}}<sup>đ</sup>
-                    @else
-                    Liên hệ
-                    @endif</h4>
-                    @if ($prd->sale_price)
-                <div class="product-discount">
-                  <h5 class="percentage-discount price">{{number_format(($prd->regular_price - $prd->sale_price)*100/$prd->regular_price, 2)}} %</h5>
-                  <h6 class="original-price price">{{$prd->regular_price}}<sup>đ</sup></h6>
-                </div>
-                @endif
-                </div>
-
-                </div>
-              </a>
-            @endif
+		  		@if($prd->is_show)
+				<div class="item">
+					<a href="{{route('front.product-detail',$prd->slug)}}">
+						<div class="product-item">
+							<div class="product-image">
+							    <figure>
+							      <img src="{{$prd->thumb}}">
+							    </figure>
+							</div>
+							<div class="product-text">
+								<h3 class="product-name">{{$prd->name}}</h3>
+								<h4 class="product-price price">
+									@if($prd->regular_price || $prd->current_price)
+										{{$prd->sale_price ? $prd->sale_price : $prd->regular_price}}<sup>đ</sup>
+									@else
+										Liên hệ
+									@endif
+								</h4>
+								@if ($prd->sale_price)
+								<div class="product-discount">
+									<h5 class="percentage-discount price">{{number_format(($prd->regular_price - $prd->sale_price)*100/$prd->regular_price, 2)}} %</h5>
+									<h6 class="original-price price">{{$prd->regular_price}}<sup>đ</sup></h6>
+								</div>
+								@endif
+							</div>
+						</div>
+					</a>
+				</div>
+				@endif
             @endforeach
           </div>
         </div>
       </div>
     </div>
- </section>
- @endif
+</section>
+@endif
