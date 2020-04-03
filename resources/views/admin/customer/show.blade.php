87c1d9dbda384b86d4916925b2e6115a911734e8
heromc <doanminhchien@gmail.com>
2020-04-03 18:53:20 +0700

    cạp nhật listing customer admin

--- resources/views/admin/customer/show.blade.php
+++ resources/views/admin/customer/show.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 
 <div>Họ và tên: {{$customer->name}}</div>
+<div>Giới tính: {{$customer->gender == 'male' ? 'Nam' : 'Nữ'}}</div>
 <div>Email: {{$customer->email}}</div>
 <div>Điện thoại: {{$customer->phone}}</div>
 
