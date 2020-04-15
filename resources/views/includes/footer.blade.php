643da4d12d510d070d71c8ccd99799330199baa1
TeeGee <thang.hoang1307@gmail.com>
2020-04-15 09:49:51 +0700

    up

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -161,21 +161,31 @@
         <div class="row">
           <div class="col-md-8 order-2 order-md-0">
             <div class="footer-bottom-detail">
-              <h3 class="footer-bottom-detail-title">Công ty TNHH Dịch Vụ UM - </h3>
-              <p class="footer-bottom-detail-content">OneStopShop IT - Lựa chọn hàng đầu về giải pháp và thiết bị trong
-                lĩnh vực công nghệ thông tin</p>
-              <p class="footer-bottom-detail-content">Số giấy chứng nhận ĐKDN: ???. Đăng ký lần đầu: ???. Cơ quan
-                cấp:???, Sở kế hoạch và đầu tư thành phố Hồ Chí Minh.</p>
+              <h3 class="footer-bottom-detail-title">OneStopShop IT - Lựa chọn hàng đầu về giải pháp và thiết bị trong
+                lĩnh vực công nghệ thông tin</h3>
+              <p class="footer-bottom-detail-content">Giấy chứng nhận ĐKDN số: 0313503636 do Sở KH &amp; ĐT TP.HCM cấp lần đầu ngày 23/10/2015.<br />
+			  Địa chỉ: Khu đô thị Vinhomes Central Park, số 208 Nguyễn Hữu Cảnh, Phường 22, Q. Bình Thạnh, TP.HCM.</p>
+			  <p class="footer-bottom-detail-content">Hotline <a href="tel:0837.000.247">0837.000.247</a> - Email <a href="mailto:cskh@onestopshop.vn">cskh@onestopshop.vn</a>. Copyright © <span id="currentyear">20</span> CÔNG TY TNHH DỊCH VỤ UM.</p>
             </div>
           </div>
           <div class="col-md-2 order-1 order-md-1">
+<<<<<<< HEAD
             <div class="image-check">
+              <!-- <a style="color: #999; text-decoration: none;" rel ="nofollow" href="https://comodosslstore.com" target="_blank">
+				<img style="width: 50%; height: 50%;" src="https://ssl.comodo.com/images/trusted-site-seal.png"
+				alt="Comodo Trusted Site Seal" style="border: 0px;"><br>
+				<span style="font-weight:bold; font-size:7pt; font-weight: normal; position: relative; top: -5px;">
+				Secured By Comodo</span></a> -->
+            </div>
+=======
+            <!--<div class="image-check">
               <a style="color: #999; text-decoration: none;" rel ="nofollow" href="https://comodosslstore.com" target="_blank">
 				<img style="width: 50%; height: 50%;" src="https://ssl.comodo.com/images/trusted-site-seal.png"
 				alt="Comodo Trusted Site Seal" style="border: 0px;"><br>
 				<span style="font-weight:bold; font-size:7pt; font-weight: normal; position: relative; top: -5px;">
 				Secured By Comodo</span></a>
-            </div>
+            </div>-->
+>>>>>>> d2e226070097b67d9bf03aa7d5b44de59013ac7d
           </div>
 		  <div class="col-md-2 order-2 order-md-2">
             <div class="image-check">
@@ -196,9 +206,36 @@
 @if(Session::has('error'))
 <div id="toast-container" class="toast-top-right"><div class="toast toast-error" aria-live="assertive" style=""><div class="toast-message">{{session('error')}}</div></div></div>
 @endif
-
+<<<<<<< HEAD
+<script src="{{asset('bower_components/admin-lte/plugins/jquery/jquery.min.js')}}"></script>
+<script>
+$(function(){
+  let myScript = document.createElement("script");
+  myScript.src = "{{asset('assets/js/main.js')}}";
+  document.body.appendChild(myScript);
+})
+  </script>
 <script src="https://getbootstrap.com/docs/4.1//assets/js/vendor/popper.min.js"></script>
+
+
+=======
+
+<!--<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>-->
+<!--<script src="https://getbootstrap.com/docs/4.1//assets/js/vendor/popper.min.js"></script>-->
+
+<!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>-->
 <script src="{{asset('assets/js/main.js')}}"></script>
+>>>>>>> d2e226070097b67d9bf03aa7d5b44de59013ac7d
 <script>
 $('#toast-container').delay(3000).fadeOut(300);
+
+$(document).ready(function () {
+  var d = new Date();
+  var n = d.getFullYear();
+  document.getElementById("currentyear").innerHTML = n;
+});
 </script>
+
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,65 +2,65 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	
+
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	
+
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 	<meta name="description" content="">
-	
+
 	<meta name="author" content="">
-	
+
 	<link rel="icon" href="/assets/img/favicon.ico">
 	@if(Request::path() == 'tin-tuc-cong-nghe')
 		@php
 		$title = "Tin tức công nghệ | One Stop Shop - Giải Pháp | Thiết Bị CNTT";
 		$desc = "Chuyên trang tin tức công nghệ";
 		@endphp
-	
+
 	@elseif(Request::path() == 'san-pham')
 		@php
 		$title = "Tất cả sản phẩm | One Stop Shop - Giải Pháp | Thiết Bị CNTT";
 		$desc = "Tất cả sản phẩm tại One Stop Shop";
 		@endphp
-	
+
 	@elseif(isset($post))
 		@php
 		$title = $post->meta_title ? $post->meta_title : $post->title ? $post->title.' | One Stop Shop - Giải Pháp | Thiết Bị CNTT' : 'One Stop Shop - Giải Pháp | Thiết Bị CNTT';
 		$desc = $post->meta_desc ? $post->meta_desc : $post->content ? substr(preg_replace( "/\r|\n/", "", html_entity_decode(strip_tags($post->content))), 0, 300).'...' : "One Stop Shop - Giải Pháp | Thiết Bị CNTT";
 		@endphp
-	
+
 	@elseif(isset($prd))
 		@php
 		$title = $prd->meta_title ? $prd->meta_title : $prd->name ? $prd->name.' | One Stop Shop - Giải Pháp | Thiết Bị CNTT' : 'One Stop Shop - Giải Pháp | Thiết Bị CNTT';
 		$desc = $prd->meta_desc ? $prd->meta_desc : $prd->long_desc ? substr(preg_replace( "/\r|\n/", "", html_entity_decode(strip_tags($prd->long_desc))), 0, 300).'...' : "One Stop Shop - Giải Pháp | Thiết Bị CNTT";
 		@endphp
-	
+
 	@elseif(isset($cat))
 		@php
 		$title = $cat->meta_title ? $cat->meta_title : $cat->name ? $cat->name.' | One Stop Shop - Giải Pháp | Thiết Bị CNTT' : 'One Stop Shop - Giải Pháp | Thiết Bị CNTT';
 		$desc = $cat->meta_desc ? $cat->meta_desc : "Các sản phẩm thuộc danh mục ".$cat->name ;
 		@endphp
-	
+
 	@elseif(isset($brand))
 		@php
 		$title = $brand->name ? 'Thương hiệu '.$brand->name.' | One Stop Shop - Giải Pháp | Thiết Bị CNTT' : ' | One Stop Shop - Giải Pháp | Thiết Bị CNTT';
 		$desc = $brand->name ? 'Các sản phẩm thuộc thương hiệu '.$brand->name : 'One Stop Shop - Giải Pháp | Thiết Bị CNTT';
 		@endphp
-	
+
 	@else
 		@php
 		$title = 'One Stop Shop - Giải Pháp | Thiết Bị CNTT';
 		$desc = 'One Stop Shop Vietnam, One Stop Shop Việt Nam, One Stop Shop .vn, OneStopShop.vn, Giải Pháp | Thiết Bị Công Nghệ Thông Tin';
 		@endphp
 	@endif
-	
+
 	<title>{{$title}}</title>
-	
+
 	<meta name="title" content="{{$title}}" />
-	
+
 	<meta name="description" content="{{$desc}}" />
-	
+
 	<link rel="canonical" href="{{Request::url()}}" />
 	<!-- Google Tag Manager -->
 	<script>(function(w, d, s, l, i) {
@@ -79,12 +79,22 @@ f.parentNode.insertBefore(j, f);
 	<!-- End Google Tag Manager -->
 
 	<!-- Bootstrap core CSS -->
+<<<<<<< HEAD
 	<link rel="preload" as="style" href="{{asset('assets/css/main.css')}}">
 	<!--<link rel="preload" as="script" href="{{asset('assets/js/main.js')}}">     -->
+
+	<link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
+	<link rel="stylesheet" href="{{asset('bower_components/admin-lte/plugins/toastr/toastr.min.css')}}">
+
+=======
+	<!--<link rel="preload" as="style" href="{{asset('assets/css/main.css')}}">-->
+	<!--<link rel="preload" as="script" href="{{asset('assets/js/main.js')}}">  -->
 	
 	<link rel="stylesheet" href="{{asset('assets/css/main.css')}}">       
 	<link rel="stylesheet" href="{{asset('bower_components/admin-lte/plugins/toastr/toastr.css')}}">
 	
+	
+>>>>>>> d2e226070097b67d9bf03aa7d5b44de59013ac7d
 </head>
 <body>
 
@@ -101,7 +111,7 @@ f.parentNode.insertBefore(j, f);
 				</div>
 			</div>
 		</div>
-		
+
 		<div class="header-user d-none d-lg-block">
 			<div class="container">
 				<div class="row justify-content-end">
@@ -123,17 +133,17 @@ f.parentNode.insertBefore(j, f);
 				</div>
 			</div>
 		</div>
-		
+
 		<div class="header-brand">
 			<div class="container">
 				<div class="row align-items-center">
 					<div class="col-md-4"><a href="{{route('front.index')}}" class="logo-brand">
 						<img src="{{$settings->find('logo')->value}}" class="img-fluid" alt=""></a>
-						
+
 						<div class="d-none d-md-block"><a href="#" class="toggle-category">
 								<i class="icon icon-align-justify"></i>
 							</a></div>
-						
+
 						<div class="d-block d-lg-none">@auth('customer')
 							<a href="{{route('front.account.index')}}" class="user-action-toggle">
 								<i class="icon icon-user-profile"></i>
@@ -145,7 +155,7 @@ f.parentNode.insertBefore(j, f);
 							</a>
 							@endguest</div>
 					</div>
-					
+
 					<div class="col-11 col-md-5">
 							<!-- Start Side Menu -->
 							<a class="menu-toggle collapsed" data-toggle="collapse" href="#collapseMenu" role="button" aria-expanded="false"
@@ -165,19 +175,19 @@ f.parentNode.insertBefore(j, f);
 											<select class="header-brand-search-filter-btn dropdown-toggle" name="cat">
 												<option value="0">Danh mục</option>
 												@foreach ($cats as $cat)
-												
+
 												<option value="{{$cat->slug}}">{{$cat->name}}</option>
 												@endforeach
 											</select>
 										</div>
 									</div>
-									
+
 									<div class="col-10 col-lg-7">
 										<div class="header-brand-search-input">
 											<input type="text" class="form-control" aria-label="Tìm kiếm sản phẩm" placeholder="Tìm kiếm sản phẩm" name="search" value="{{app('request')->input('search')}}">
 										</div>
 									</div>
-									
+
 									<div class="col-2 col-lg-1">
 										<button type="submit" class="header-brand-search-action bg-transparent border-0"><i class="icon icon-search"></i></button>
 									</div>
@@ -185,7 +195,7 @@ f.parentNode.insertBefore(j, f);
 							</form>
 						</div>
 					</div>
-					
+
 					<div class="col-1 col-md-3">
 						<div class="text-right">
 							<!--USER ACTION-->
@@ -197,7 +207,7 @@ f.parentNode.insertBefore(j, f);
 								<a class="user-cart d-none d-md-inline-block" data-toggle="modal" data-target="#loginModal"><span>Đăng nhập/Đăng ký <i
 								class="icon icon-user-head"></i></span></a>
 							@endguest
-							
+
 								<!--USER LOGIN-->
 								<span class="cart-title d-none d-md-inline-block">
 									@if(session()->get('cart.items'))
@@ -206,18 +216,18 @@ f.parentNode.insertBefore(j, f);
 									<a href="#">Giỏ hàng</a>
 									@endif
 								</span>
-							
+
 							<div class="dropdown dropdown-discount">@if(session()->get('cart.items'))
 									<a class="highlight d-block d-md-none" href="{{route('front.check_out_1')}}">
-										
+
 										<span class="header-cart-number">{{array_sum(session()->get('cart.items'))}}</span>
-										
+
 										<i class="icon icon-shopping-cart"></i>
 									</a>
 									@else
 									<a class="highlight d-block d-md-none" href=""><i class="icon icon-shopping-cart"></i></a>
 									@endif
-									
+
 								@if(session()->get('cart.items'))
 								<a class="highlight d-none d-md-block" href="{{route('front.check_out_1')}}" id="dropdownMenuButton" data-toggle="dropdown"
 									aria-haspopup="true"
@@ -232,7 +242,7 @@ f.parentNode.insertBefore(j, f);
 									<i class="icon icon-shopping-cart"></i>
 								</a>
 								@endif
-								
+
 								<!-- quick cart -->
 								<div class=" d-none d-md-block">
 									<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -243,7 +253,7 @@ f.parentNode.insertBefore(j, f);
 												<div class="product-head"><a href="{{route('front.product-detail',$prd->slug)}}" class="card-image">
 													<img src="{{$prd->thumb}}" class="img-fluid">
 													</a>
-													
+
 													<h3 class="product-head-title"><a href="{{route('front.product-detail',$prd->slug)}}">{{$prd->name}}</a></h3>
 													<span class="product-price price">{{ $prd->current_price}}<sup>đ</sup><strong class="count"> x{{$carts[$prd->id]}}</strong></span></div>
 											</li>
@@ -253,7 +263,7 @@ f.parentNode.insertBefore(j, f);
 											@endif
 										</ul>
 										@if(count($prds_in_cart))
-										
+
 										<div class="product-end">
 											<h5 class="product-end-block"><span class="product-end-title">Thành tiền</span> <span
 												class="product-end-block-price price">{{$cart_total}}<sup>đ</sup></span></h5>
@@ -268,7 +278,7 @@ f.parentNode.insertBefore(j, f);
 				</div>
 			</div>
 		</div>
-		
+
 		<div class="d-none d-md-block">
 			<div class="header-category">
 				<div class="container">
@@ -287,11 +297,11 @@ f.parentNode.insertBefore(j, f);
 	<div class="collapse hidden-mobile" id="collapseMenu">
 		<div class="hidden-bg menu-toggle" class="" data-toggle="collapse" href="#collapseMenu" role="button" aria-expanded="true"
         aria-controls="collapseMenu" style="background-color: rgba(0,0,0,0.75); width: 15vw; height: 100%; position: absolute; left: 85vw; top: 0; z-index: 19;">&nbsp;</div>
-		
+
 		<div class="hidden-panel-content"><a href="{{route('front.product-list')}}" class="product-navigation-action">
 				<i class="icon icon-align-justify"></i> Danh mục sản phẩm
 			</a>
-			
+
 			<ul class="hidden-panel-content-menu">
 				@foreach ( $cats as $cat )
 				<li><a class="header-category-item" href="{{route('front.category-list',$cat->slug)}}">
