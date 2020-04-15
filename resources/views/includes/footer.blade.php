387ff34765275a6aece57d0e39d3720cc7aabd53
heromc <doanminhchien@gmail.com>
2020-04-15 14:10:37 +0700

    up

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -240,7 +240,7 @@
 <!--<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>-->
 <!--<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>-->
-
+<!--<script src="https://getbootstrap.com/docs/4.1//assets/js/vendor/popper.min.js"></script>-->
 <!-- Site JS -->
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
@@ -254,15 +254,14 @@
 <script src="assets/js/jquery.nicescroll.min.js"></script>
 <script src="assets/js/sticky-sidebar.min.js"></script>
 <script src="assets/js/bootstrap-multiselect.min.js"></script>
+<!--<script src="assets/js/main.js"></script>-->
 <script>
 $(function() {
 	let myScript = document.createElement("script");
 	myScript.src = "{{asset('assets/js/main.js')}}";
 	document.body.appendChild(myScript);
 })
-  </script>
-<!--<script src="https://getbootstrap.com/docs/4.1//assets/js/vendor/popper.min.js"></script>-->
-
+</script>
 
 <script>
 $('#toast-container').delay(3000).fadeOut('fast');
